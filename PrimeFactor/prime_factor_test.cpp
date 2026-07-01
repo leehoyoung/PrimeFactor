a7f5afa4c8cff09b0859af268df9f2e4ac42a439
Hoyoung Lee <hyl4237@gmail.com>
2026-07-01 09:15:54 +0900

    [feature] 02: Add Of2 UT and code
    Signed-off-by: Hoyoung Lee <hyl4237@gmail.com>

--- PrimeFactor/prime_factor_test.cpp
+++ PrimeFactor/prime_factor_test.cpp
@@ -6,3 +6,9 @@ TEST(PrimeFactor, PrimeTestOf1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactor, PrimeTestOf2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
