5c9b3970865884ac23d1a61335777b3957f1c534
Hoyoung Lee <hyl4237@gmail.com>
2026-07-01 09:55:20 +0900

    [feature] 07
    Signed-off-by: Hoyoung Lee <hyl4237@gmail.com>

--- PrimeFactor/prime_factor_test.cpp
+++ PrimeFactor/prime_factor_test.cpp
@@ -37,3 +37,8 @@ TEST_F(PrimeFixture, PrimeTestOf9) {
 	expected = { 3, 3 };
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
+
+TEST_F(PrimeFixture, PrimeTestOf12) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
